00abbf1e8c704e986264289db3e26b5b706e0097
Rajpriya <rajpriyarj75@gmail.com>
2020-12-05 23:18:37 +0530

    Updates

---
+++ precache-manifest.50e991750ac5bb0e53f6278fd80e8d1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4486da3971302d6ddb0e6b0351f4ce8e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "41667bf4c57629c8f9df",
+    "url": "/portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "01d93dcb8072a4017916",
+    "url": "/portfolio/static/js/2.4143022c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.4143022c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41667bf4c57629c8f9df",
+    "url": "/portfolio/static/js/main.67252438.chunk.js"
+  },
+  {
+    "revision": "039eb055909a0ac81ca5",
+    "url": "/portfolio/static/js/runtime-main.3e7fea94.js"
+  },
+  {
+    "revision": "66c9ac0acd11da4711ab2ae8dc0deef1",
+    "url": "/portfolio/static/media/profile.66c9ac0a.jpg"
+  }
+]);
